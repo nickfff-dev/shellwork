077e3d1bab3f40241dcc4e02391c0347dc9a352d
nickfff-dev <hunchographics@gmail.com>
2023-05-27 04:12:31 +0300

    first commit

--- _strcat.c
+++ _strcat.c
@@ -12,20 +12,16 @@ char *_strcat(char *dest, char *src)
 {
     int i = 0;
     int j = 0;
+ 
 
-    while (dest[i] != '\0') /* Initialize i before using it */
+   for (i = 0; dest[i] != '\0'; i++)
     {
-        i++;
+        ;
     }
-
-    while (src[j] != '\0')
+    for (j = 0; src[j] != '\0'; j++)
     {
-        dest[i] = src[j];
-        i++;
-        j++;
+        dest[i + j] = src[j];
     }
-
-    dest[i] = '\0';
-
+    dest[i + j] = '\0';
     return (dest);
 }
