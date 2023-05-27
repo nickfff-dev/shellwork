724abbca00ac680b8c12148751f760d3266656c3
nickfff-dev <hunchographics@gmail.com>
2023-05-27 19:37:27 +0300

    waz

--- execute_command.c
+++ execute_command.c
@@ -12,21 +12,21 @@ int execute_command(char **array)
 	if (array[0][0] == '/')
 	{
 		status = execute_external_command(array);
-		free(array);
+		free_array(array);
 		return (status);
 	}
 	if (_strcmp(array[0], "env") == 0)
 	{
 		status = execute_builtin_env();
-		free(array);
+		free_array(array);
 		return (status);
 	}
 	if (_strcmp(array[0], "exit") == 0)
 	{
-		free(array);
+		free_array(array);
 		exit(0);
 	}
 	status = execute_path_commands(array);
-	free(array);
+	free_array(array);
 	return (status);
 }
